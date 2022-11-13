708bceb15ce83aead1530e21edefe9e1ef7263b4
Tomas Alegria Caceres <tomialegriacaceres@gmail.com>
2022-11-13 11:25:40 -0300

    almost

--- api-movie/src/main/java/com/dh/movie/ApiMovieApplication.java
+++ api-movie/src/main/java/com/dh/movie/ApiMovieApplication.java
@@ -7,10 +7,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableDiscoveryClient
 public class ApiMovieApplication {
 
     public static void main(String[] args) {
