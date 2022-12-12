4e2376f100c532784a3ac4fa090ac6c260ce8c7c
Tomas Alegria Caceres <tomialegriacaceres@gmail.com>
2022-12-12 12:22:43 -0300

    fixed series consumer

--- api-catalog/src/main/java/com/dh/catalog/event/NewSeriesEventConsumer.java
+++ api-catalog/src/main/java/com/dh/catalog/event/NewSeriesEventConsumer.java
@@ -21,22 +21,7 @@ public class NewSeriesEventConsumer {
     }
 
     @RabbitListener(queues = RabbitMQConfig.QUEUE_NEW_SERIES)
-    public void execute(SeriesDTO seriesDTO) {
-        Series series = new Series();
-        BeanUtils.copyProperties(seriesDTO, series);
-/*        for (SeasonDTO sDTO :
-                seriesDTO.getSeasonsDTO()) {
-            Season season = new Season();
-            BeanUtils.copyProperties(sDTO, season);
-            for (ChapterDTO cDTO :
-                    sDTO.getChaptersDTO()) {
-                Chapter chapter = new Chapter();
-                BeanUtils.copyProperties(cDTO, chapter);
-                season.getChapters().add(chapter);
-            }
-            series.getSeasons().add(season);
-        }*/
-
+    public void execute(Series series) {
         seriesRepository.deleteById(series.getSerieId());
         seriesRepository.save(series);
     }
