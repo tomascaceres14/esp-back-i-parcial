9a021635bdc0e31abc98877f698ed91e2011078a
Tomas Alegria Caceres <tomialegriacaceres@gmail.com>
2022-12-12 12:23:27 -0300

    uwu

--- api-catalog/src/main/java/com/dh/catalog/event/NewSeriesEventConsumer.java
+++ api-catalog/src/main/java/com/dh/catalog/event/NewSeriesEventConsumer.java
@@ -1,14 +1,9 @@
 package com.dh.catalog.event;
+
 import com.dh.catalog.config.RabbitMQConfig;
-import com.dh.catalog.model.Chapter;
-import com.dh.catalog.model.Season;
 import com.dh.catalog.model.Series;
-import com.dh.catalog.model.dto.ChapterDTO;
-import com.dh.catalog.model.dto.SeasonDTO;
-import com.dh.catalog.model.dto.SeriesDTO;
 import com.dh.catalog.repository.SeriesRepository;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 @Component
